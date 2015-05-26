ebf5d92fab7746080278dd7eb78f101ed6874e0d
sebi0920 <i.plangger@gmail.com>
2015-05-26 23:33:57 +0200

    Chronometer
    Just a test for the timer app, a simple chronometer with a start&stop
    button, in the chrono format: "HH:MM:SS"
    
    Maybe it helps you at some point in your projects!
    
    Cheers everyone!

---
+++ Chrono/app/src/main/java/com/spla/chrono_test/MainActivity.java
@@ -0,0 +1,73 @@
+package com.spla.chrono_test;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Chronometer chronometer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = (Chronometer)findViewById(R.id.chronometer);
+        chronometer.setFormat("00:00:00");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void button_start(View view){
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer cArg) {
+                long time = SystemClock.elapsedRealtime() - cArg.getBase();
+                int h = (int) (time / 3600000);
+                int m = (int) (time - h * 3600000) / 60000;
+                int s = (int) (time - h * 3600000 - m * 60000) / 1000;
+                String hh = h < 10 ? "0" + h : h + "";
+                String mm = m < 10 ? "0" + m : m + "";
+                String ss = s < 10 ? "0" + s : s + "";
+                cArg.setText(hh + ":" + mm + ":" + ss);
+            }
+        });
+
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
+    }
+
+    public void button_stop(View view){
+        chronometer.stop();
+    }
+}
